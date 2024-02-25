dbad029f5edc673244c36189764c5e0d44d1bf96
btb0017 <btb0017@mix.wvu.edu>
2024-02-25 18:58:52 -0500

    Updated code for DB Creation and Data

--- DatabaseCreation.sql
+++ DatabaseCreation.sql
@@ -6,33 +6,33 @@ GO
 
 CREATE TABLE Company (
     CompanyID INT PRIMARY KEY,
-    CompanyName NVARCHAR(50),
-    CompanyStreet NVARCHAR(50),
-    CompanyCity NVARCHAR(MAX),
-    CompanyState NVARCHAR(MAX),
-    CompanyCountry NVARCHAR(50),
-    CompanyZip NVARCHAR(10),
-    CompanyEmail NVARCHAR(MAX),
-    CompanyLatitude NVARCHAR(MAX),
-    CompanyLongitude NVARCHAR(MAX),
-    CompanyWebsite NVARCHAR(MAX),
-    CompanyClimateRating INT
+    CompanyName NVARCHAR(50) not null,
+    CompanyStreet NVARCHAR(50) not null,
+    CompanyCity NVARCHAR(MAX) not null,
+    CompanyState NVARCHAR(MAX) not null,
+    CompanyCountry NVARCHAR(50) not null,
+    CompanyZip NVARCHAR(10) not null,
+    CompanyEmail NVARCHAR(MAX) not null,
+    CompanyLatitude NVARCHAR(MAX) not null,
+    CompanyLongitude NVARCHAR(MAX) not null,
+    CompanyWebsite NVARCHAR(MAX) not null,
+    CompanyClimateRating INT not null
 );
 GO
 
 CREATE TABLE Date (
     DateID INT PRIMARY KEY IDENTITY(1,1),
-    DateValue DATE UNIQUE
+    DateValue DATE UNIQUE not null
 );
 GO
 
 CREATE TABLE ClimateData (
     ClimateDataID INT PRIMARY KEY IDENTITY(1,1),
-    CompanyID INT,
-    DateID INT,
-    Temperature FLOAT,
-    Humidity FLOAT,
-    CO2Levels FLOAT,
+    CompanyID INT not null,
+    DateID INT not null,
+    Temperature FLOAT not null,
+    Humidity FLOAT not null,
+    CO2Levels FLOAT not null,
     FOREIGN KEY (CompanyID) REFERENCES Company(CompanyID),
     FOREIGN KEY (DateID) REFERENCES Date(DateID)
 );
@@ -40,13 +40,13 @@ GO
 
 CREATE TABLE StockData (
     StockDataID INT PRIMARY KEY IDENTITY(1,1),
-    CompanyID INT,
-    DateID INT,
-    OpeningPrice FLOAT,
-    ClosingPrice FLOAT,
-    High FLOAT,
-    Low FLOAT,
-    Volume INT,
+    CompanyID INT not null,
+    DateID INT not null,
+    OpeningPrice FLOAT not null,
+    ClosingPrice FLOAT not null,
+    High FLOAT not null,
+    Low FLOAT not null,
+    Volume INT not null,
     FOREIGN KEY (CompanyID) REFERENCES Company(CompanyID),
     FOREIGN KEY (DateID) REFERENCES Date(DateID)
 );
