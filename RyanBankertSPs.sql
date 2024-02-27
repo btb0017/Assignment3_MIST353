3b6879520417f7412a8ea9ab274e6d9551a874fd
ryanbankert <142524806+ryanbankert@users.noreply.github.com>
2024-02-26 22:53:43 -0500

    Committing my stored procedures to repo

---
+++ RyanBankertSPs.sql
@@ -0,0 +1,38 @@
+USE EcoInvest_DB;
+GO
+/* Stored Procedure #1: Get all details for a company by companyID. Used to generate data for a company’s detailed information page.*/
+CREATE PROCEDURE spCompanyDetails
+	@CompanyID int
+	AS
+	BEGIN
+		SELECT * FROM Company WHERE CompanyID = @CompanyID;
+	END;
+	GO
+/*
+EXEC spCompanyDetails 
+    @CompanyID = 1, 
+GO
+*/
+
+/* Stored Procedure #2: Add a company. Used to add company details to track information.*/
+CREATE PROCEDURE spCompanyAdd
+	@CompanyID int,
+	@CompanyName nvarchar(50),
+	@CompanyStreet nvarchar(50),
+	@CompanyCity nvarchar(MAX),
+	@CompanyState nvarchar(MAX),
+	@CompanyCountry nvarchar(50),
+	@CompanyZip nvarchar(10),
+	@CompanyEmail nvarchar(MAX),
+	@CompanyLatitude nvarchar(MAX),
+	@CompanyLongitude nvarchar(MAX),
+	@CompanyWebsite nvarchar(MAX),
+	@CompanyClimateRating int
+		AS
+		BEGIN
+		INSERT INTO Company(CompanyID, CompanyName, CompanyStreet, CompanyCity, CompanyState, CompanyCountry, 
+			CompanyZip, CompanyEmail, CompanyLatitude,CompanyLongitude,CompanyWebsite, CompanyClimateRating) 
+		VALUES (@CompanyID, @CompanyName, @CompanyStreet, @CompanyCity, @CompanyState, @CompanyCountry, 
+			@CompanyZip, @CompanyEmail, @CompanyLatitude, @CompanyLongitude , @CompanyWebsite , @CompanyClimateRating);
+		END;
+/* EXEC spCompanyAdd */
